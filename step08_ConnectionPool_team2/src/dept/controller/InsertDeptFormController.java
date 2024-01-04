0bdefa9e62ac412ec65a0c8ff62cbbcc0fd4c3ee
lucia17 <dkdldb9867@gmail.com>
2024-01-04 16:31:56 +0900

    insertDept 부서 추가기능
    Team2
    
    2024-01-04 이예림

--- step08_ConnectionPool_team2/src/dept/controller/InsertDeptFormController.java
+++ step08_ConnectionPool_team2/src/dept/controller/InsertDeptFormController.java
@@ -16,7 +16,21 @@ import dept.dto.Dept;
 @WebServlet("/insertDeptForm.do")
 public class InsertDeptFormController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// ?
+		// 로그인 검증
+//		HttpSession session = request.getSession(false);
+//		if(session == null) {
+//			response.sendRedirect("login.jsp");
+//			return;
+//		}	
+				
+		
+		//
+		String url = "errors/error.jsp";
+		
+		url = "dept/insertDept.jsp";
+		request.getRequestDispatcher(url).forward(request, response);
+		
+		
 		
 	}
 }
