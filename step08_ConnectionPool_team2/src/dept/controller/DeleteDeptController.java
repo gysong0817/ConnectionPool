19231934a3d7800bdbc37af99d57e731048954e2
wire987 <wire987@naver.com>
2024-01-04 16:31:29 +0900

    deleteDept 변경

--- step08_ConnectionPool_team2/src/dept/controller/DeleteDeptController.java
+++ step08_ConnectionPool_team2/src/dept/controller/DeleteDeptController.java
@@ -16,6 +16,36 @@ import dept.dto.Dept;
 public class DeleteDeptController extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// ?
+		String url = "errors/error.jsp";
+		int deptno = Integer.parseInt(request.getParameter("deptno"));
+
+		// step01 : 해당 deptno로 객체 확인
+		Dept dept = null;
+		boolean result = false;
 		
+		try {
+			dept = DeptDAO.getDeptByDeptno(deptno);
+			
+			if (dept == null) {
+				request.setAttribute("error", "존재 하지 않는 부서");
+				request.getRequestDispatcher(url).forward(request, response);
+			} else {
+				// step02 : 삭제 로직
+				result = DeptDAO.deleteDeptByDeptno(deptno);
+			}
+			
+			// step 03 : 페이지 이동
+			if (result) {
+				url = "getDeptList.do";
+				response.sendRedirect(url);
+				return;
+			} else {
+				request.setAttribute("error", "부서 삭제 실패");
+				request.getRequestDispatcher(url).forward(request, response);
+			}
+		} catch (SQLException e) {
+			request.setAttribute("error", "부서 삭제 실패");
+			request.getRequestDispatcher(url).forward(request, response);
+		}
 	}
 }
